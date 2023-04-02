afc608343183d11ca2a050045e672234c25bee41
Miging <dnfldpden32@gmail.com>
2023-04-02 18:44:48 +0900

    change

--- P4.py
+++ P4.py
@@ -23,24 +23,52 @@ def prior(n):
         return 3
     elif n=='+' or n=='-':
         return 2
+    elif n=='(':
+        return 1
+def recognize(input):
+    recognized=['']
+    numbers=list('1234567890.')
+    i=0
+    while i<len(input):
+       j=1
+       if input[i] in numbers:
+           while i+j<len(input):
+               if input[i+j] in numbers:
+                    j+=1
+               else:
+                   break
+       else:
+            recognized.append(''.join(input[i:i+j]))
+            i+=j
+    return recognized
+
     
 num=Stack()
-post=Stack()
+post=[]
 input=input()
 token=0
+recognized=recognize(input)
 #후위식으로 변환
 #숫자는 그냥 저장
 #문자가 나오면 스택에 쌓고, 그다음 문자가 나오면 그 전 문자와의 우선 순위를 비교후 낮다면  
 for i in input:
-    #연산자라면
-    if i=='+' or i=='-' or i=='/' or i=='*':
-        #전에 있던 연산자보다 우선순위가 낮다면 그 출력
-        token=0
-        while(post.top() and prior(post.top())>=prior(i)):
-            e=post.pop()
-            print(e,end='')
-        post.push(i)
+    #여는괄호는 바로 넣음
+    if i=='(':
+        num.push(i)
+    elif i==')':
+        #(가 나올때까지 연산자 추출
+        while num.top()!='(':
+            post.append(num.pop())
+        #여는 괄호는 그냥 뺌
+        num.pop()
+    elif i=='+' or i=='-' or i=='/' or i=='*':
+        #top에 있는 연산자가 현재
+        while(not num.isEmpty() and prior(num.top())>=prior(i)):
+            e=num.pop()
+            post.append(e)
+        num.push(i)
     else:
-        print(i,end='')
-while not post.isEmpty():
-    print(post.pop(),end='')
+
+        post.append(i)
+while not num.isEmpty():
+    post.append(num.pop())
